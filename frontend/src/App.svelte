7dd2390853871eb3c5abe6ac11b8e7ef1e9929ff
alex <aok@buran.rest>
2022-03-20 20:09:38 +0300

    bootstrap nav works

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -9,18 +9,6 @@
 	import Sidenav from './app/component/Sidenav.svelte';
 </script>
 
-<style>
-  .app-shell {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-  }
-</style>
 
-<div class="app-shell">
-  <Sidenav class="sidenav" />
-  <Router />
-</div>
+<Sidenav class="sidenav" />
+<Router />
