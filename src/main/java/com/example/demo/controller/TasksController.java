979a92bfffb0c9829b3827982d82304ed82711db
alex <aok@buran.rest>
2022-03-23 10:57:39 +0300

    bootstrap start

--- src/main/java/com/example/demo/controller/TasksController.java
+++ src/main/java/com/example/demo/controller/TasksController.java
@@ -4,11 +4,8 @@ import com.example.demo.entities.Tasks;
 import com.example.demo.service.TasksService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -5,11 +5,13 @@ import com.example.demo.form.TaskForm;
 import com.example.demo.service.TasksService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -120,4 +122,5 @@ public class WebController {
         // возвращаем шаблон главной страницы
         return "index";
     }
+
 }
